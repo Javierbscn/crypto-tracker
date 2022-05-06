1b39d68bfc53a16cd5fa26d5da33b9ec295226b2
Javier B <javierboscan16@gmail.com>
2022-05-06 17:18:46 -0400

    creating principally methods in notes.component

--- src/app/pages/notes/notes.component.ts
+++ src/app/pages/notes/notes.component.ts
@@ -10,12 +10,33 @@ import { LocalStorageService } from 'src/app/services/local-storage.service';
 })
 export class NotesComponent implements OnInit {
   notes$: Observable<Note[]>;
+  currentNote!: Note;
+  editingNote: boolean;
 
   constructor(private localStorageSvc: LocalStorageService) {
     this.notes$ = localStorageSvc.notes$.asObservable();
+
+    this.editingNote = false;
+
+    this.clearCurrentNote();
   }
 
   ngOnInit(): void {
   }
 
+  private clearCurrentNote(): void {
+    this.currentNote = { id: 0, title: '', description: '' };
+  }
+
+  onAddNote(): void { }
+
+  onEditingNote(note: Note): void {
+    this.currentNote = note;
+
+    this.editingNote = true;
+  }
+
+  onUpdateNote(): void { }
+
+  onDeleteNote(note: Note): void { }
 }
