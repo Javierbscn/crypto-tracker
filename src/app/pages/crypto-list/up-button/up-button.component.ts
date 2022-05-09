bb0153f6b052b2bf6c4bc246b8a882d62b1202d8
Javier B <javierboscan16@gmail.com>
2022-05-09 19:29:43 -0400

    adding up-button component in crypto-list

--- src/app/pages/crypto-list/up-button/up-button.component.ts
+++ src/app/pages/crypto-list/up-button/up-button.component.ts
@@ -1,13 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, HostListener, Inject, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-up-button',
-  templateUrl: './up-button.component.html',
+  template: `<div
+    *ngIf="showButton"
+    (click)="onScrollTop()"
+    class="position-fixed bottom-0 end-0 m-3"
+  >
+    <button class="btn btn-primary rounded-circle fs-4">⮝</button>
+  </div>`,
   styleUrls: ['./up-button.component.scss']
 })
 export class UpButtonComponent implements OnInit {
+  showButton: boolean;
 
-  constructor() { }
+  private scrollHeight: number;
+
+  constructor(@Inject(DOCUMENT) private document: Document) {
+    this.showButton = false;
+    this.scrollHeight = 500;
+  }
+
+  @HostListener('window: scroll')
+  onWindowScroll(): void {
+    const yOffSet = window.pageYOffset;
+    const scrollTop = this.document.documentElement.scrollTop;
+    this.showButton = (yOffSet || scrollTop) > this.scrollHeight;
+  }
+
+  onScrollTop(): void {
+    this.document.documentElement.scrollTop = 0;
+  }
 
   ngOnInit(): void {
   }
