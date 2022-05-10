fbdc98bb68c07a23f271d5045aabf3c8498e96b4
Javier B <javierboscan16@gmail.com>
2022-05-10 17:07:33 -0400

    adding pagination component

--- src/app/pages/crypto-list/pagination/pagination.component.ts
+++ src/app/pages/crypto-list/pagination/pagination.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, DoCheck, OnInit } from '@angular/core';
+import { CoingeckoApiService } from 'src/app/services/coingecko-api.service';
 
 @Component({
   selector: 'app-pagination',
-  templateUrl: './pagination.component.html',
-  styleUrls: ['./pagination.component.scss']
+  template: `<nav aria-label="Page navigation">
+    <ul class="pagination">
+      <li class="page-item" [ngClass]="{ disabled: page === 1 }">
+        <button class="page-link" (click)="onPrevPage()">Prev</button>
+      </li>
+      <li class="page-item disabled">
+        <p class="page-link">{{ page }}</p>
+      </li>
+      <li class="page-item" [ngClass]="{ disabled: page === totalPages }">
+        <button class="page-link" (click)="onNextPage()">Next</button>
+      </li>
+    </ul>
+  </nav>`,
+  styleUrls: ['./pagination.component.scss'],
 })
-export class PaginationComponent implements OnInit {
+export class PaginationComponent implements OnInit, DoCheck {
+  page: number;
+  totalPages: number;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private readonly coingeckoSvc: CoingeckoApiService) {
+    this.page = coingeckoSvc.getPage;
+    this.totalPages = coingeckoSvc.getTotalPages;
   }
 
+  ngDoCheck(): void {
+    this.page = this.coingeckoSvc.getPage;
+  }
+
+  ngOnInit(): void {}
+
+  onPrevPage(): void {
+    if (this.page === 1) return;
+
+    this.coingeckoSvc.setPage = this.page - 1;
+  }
+
+  onNextPage(): void {
+    if (this.page === this.totalPages) return;
+
+    this.coingeckoSvc.setPage = this.page + 1;
+  }
 }
