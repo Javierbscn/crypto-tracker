4f0cc799631bdb6532671bca46bfb149407e5176
Javier B <javierboscan16@gmail.com>
2022-05-10 18:27:21 -0400

    creating currency view and fixing method getter in currencies service

--- src/app/pages/crypto-list/currency/currency.component.ts
+++ src/app/pages/crypto-list/currency/currency.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Currency } from 'src/app/interfaces/currency';
+import { CoingeckoApiService } from 'src/app/services/coingecko-api.service';
+import { CurrenciesService } from 'src/app/services/currencies.service';
 
 @Component({
   selector: 'app-currency',
-  templateUrl: './currency.component.html',
-  styleUrls: ['./currency.component.scss']
+  template: `<ul class="nav nav-tabs">
+    <li class="nav-item dropdown">
+      <a
+        class="nav-link dropdown-toggle"
+        data-bs-toggle="dropdown"
+        href="#"
+        role="button"
+        aria-expanded="false"
+      >
+        Currency: {{ currency }}
+      </a>
+      <ul class="dropdown-menu">
+        <li *ngFor="let currency of currencies">
+          <button
+            class="dropdown-item"
+            (click)="onSetCurrency(currency.symbol)"
+          >
+            <span class="fw-bold">{{ currency.symbol }}</span> |
+            {{ currency.name }}
+          </button>
+        </li>
+      </ul>
+    </li>
+  </ul>`,
+  styleUrls: ['./currency.component.scss'],
 })
 export class CurrencyComponent implements OnInit {
+  @Input() currency!: Currency;
+  currencies: Currency[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private readonly currenciesSvc: CurrenciesService,
+    private readonly coingeckoApiSvc: CoingeckoApiService
+  ) {
+    this.currencies = currenciesSvc.getCurrencies;
   }
 
+  ngOnInit(): void {}
+
+  onSetCurrency(symbol: string): void {
+    console.log(symbol)
+  }
 }
--- src/app/services/currencies.service.ts
+++ src/app/services/currencies.service.ts
@@ -57,7 +57,7 @@ export class CurrenciesService {
     ];
   }
 
-  getCurrencies(): Currency[] {
+  get getCurrencies(): Currency[] {
     return this.currencies;
   }
 }
