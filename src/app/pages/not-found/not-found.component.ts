0ab65dd2a414dba92178875d22fa3d9a62a0cdc2
Javier B <javierboscan16@gmail.com>
2022-05-11 18:11:19 -0400

    creating not-found component

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
-  styleUrls: ['./not-found.component.scss']
+  template: `<section
+    class="height-75 d-flex flex-column justify-content-center align-items-center my-5 py-5"
+  >
+    <h1 class="h1 fw-bold">¡Oh no! Error 404</h1>
+    <p class="h3">The page you're looking for could not be found.</p>
+  </section>`,
+  styleUrls: ['./not-found.component.scss'],
 })
-export class NotFoundComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class NotFoundComponent {}
