93e89bc99d9c3b8d2ab67ddbbc2c928fbcdd483f
Javier B <javierboscan16@gmail.com>
2022-05-11 19:15:13 -0400

    improving view of header component

--- src/app/components/shared/header/header.component.ts
+++ src/app/components/shared/header/header.component.ts
@@ -1,19 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, DoCheck } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   template: `<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
-      <a class="navbar-brand" [routerLink]="['/']">Crypto Tracker</a>
-      <ul class="navbar-nav">
+      <a class="navbar-brand" [routerLink]="['/']"
+        ><img class="icon" src="/assets/icon.png" alt="Logo" /> Crypto
+        Tracker</a
+      >
+      <ul class="nav">
         <li class="nav-item">
-          <a class="nav-link" [routerLink]="['/']">Home</a>
+          <a
+            class="nav-link nav-link-hover"
+            [ngClass]="{
+              active: currentRoute === '/' || currentRoute === '/home'
+            }"
+            [routerLink]="['/home']"
+            >Home</a
+          >
         </li>
         <li class="nav-item">
-          <a class="nav-link" [routerLink]="['/notes']">My notes</a>
+          <a
+            class="nav-link nav-link-hover"
+            [ngClass]="{ active: currentRoute === '/notes' }"
+            [routerLink]="['/notes']"
+            >My notes</a
+          >
         </li>
       </ul>
     </div>
   </nav>`,
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {}
+export class HeaderComponent implements DoCheck {
+  currentRoute: string;
+
+  constructor(private router: Router) {
+    this.currentRoute = router.url;
+  }
+
+  ngDoCheck(): void {
+    this.currentRoute = this.router.url;
+  }
+}
